7780fd2ea56ad31d7213bb19a0eaeec826702f52
sdh <noggame64@gmail.com>
2019-01-01 04:59:33 +0900

    add VIKENDI to DeterminePlace.

--- commands/Map/determinePlace.js
+++ commands/Map/determinePlace.js
@@ -9,11 +9,8 @@ module.exports = class determinePlace extends commando.Command{
             group : 'map',
             memberName : 'map',
             description : 'determine falling place',
-            examples : ['!map --- Choose one place',
-            /*'!map list --- Show list',
-            '!map a --- Choose one place within dangerous zone.',
-            '!map b --- Choose one place within noraml zone.',
-            '!map c --- Choose one place within safe zone.',*/
+            examples : ['!mark --- Choose one place',
+            // '!map list --- Show list'
             '!mark er --- mark coordinates on the ERANGEL.',
             '!mark mr --- mark coordinates on the MIRAMAR.',
             '!mark sn --- mark coordinates on the SANHOK.',
@@ -34,8 +31,8 @@ module.exports = class determinePlace extends commando.Command{
 
         const imagePath = './resource/images';
         const RECTANGLE_SIZE = 336;
-        const MAP_SIZE = RECTANGLE_SIZE*8;
-
+        
+        let MAP_SIZE = RECTANGLE_SIZE*8;
         let map_row;
         let map_col;
         let except_coor;
@@ -45,36 +42,38 @@ module.exports = class determinePlace extends commando.Command{
         if(args.map == 'er' || args.map == 'mr' || args.map == 'sn' || args.map == 'vk'){
             if(args.map == 'er'){
                 map_name = 'ERANGEL';
-                map_row = ['A', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
+                map_row = ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
                 map_col = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-                except_coor = ['AA', 'AB', 'AC', 'AD', 'AG', 'AH', 'JA', 'JH', 'KA', 'KG', 'KH'
-                , 'LA', 'NH', 'MA', 'MH', 'NH', 'OH', 'PA', 'PB', 'PC', 'PF', 'PG', 'PH'];
+                except_coor = ['IA', 'IB', 'IC', 'ID', 'IE', 'IG', 'IH',
+                'JA', 'JH', 'KA', 'KH', 'LA', 'LH', 'NH',
+                'MA', 'MH', 'NH', 'OH', 'PA', 'PB', 'PC', 'PF', 'PG', 'PH'];
             }
             else if(args.map == 'mr'){
                 map_name = 'MIRAMAR';
-                map_row = ['A', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
+                map_row = ['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'];
                 map_col = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
                 except_coor = ['AE', 'AF', 'AG', 'AH', 'JA', 'JB', 'MH', 'NH', 'OH', 'PG', 'PH'];
             }
             else if(args.map == 'sn'){
                 map_name = 'SANHOK';
-                map_row = ['A1', 'A2', 'J1', 'J2', 'K1', 'K2', 'L1', 'L2']; // (A, J, K, L)/2
+                map_row = ['I1', 'I2', 'J1', 'J2', 'K1', 'K2', 'L1', 'L2']; // (I, J, K, L)/2
                 map_col = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2']; // (A, B, C, D)/2
 
-                except_coor = ['A1A1', 'A1A2', 'A1B1', 'A1B2', 'A1C1', 'A1C2', 'A1D1', 'A1D2'
-                ,'A2A1', 'A2A2', 'A2D1', 'A2D2', 'J1A1', 'J2A1', 'K1A1', 'L1A1', 'L1D2'
-                ,'L2A1', 'L2A2', 'L2C1', 'L2C2', 'L2D1', 'L2D2'];
+                except_coor = ['I1A1', 'I1A2', 'I1B1', 'I1B2', 'I1C1', 'I1C2', 'I1D1', 'I1D2'
+                ,'I2A1', 'I2A2', 'I2D2', 'J1A1', 'J2A1', 'K1A1', 'K1D2', 'K2A1', 'K2D2', 
+                ,'L1A1', 'L1A2', 'L1D2', 'L2A1', 'L2A2', 'L2C2', 'L2D1', 'L2D2'];
             }
             else if(args.map == 'vk'){
                 map_name = 'VIKENDI';
-                map_row = ['A1', 'A2', 'J1', 'J2', 'K1', 'K2', 'L1', 'L2']; // (A, J, K, L)/2
-                map_col = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2']; // (A, B, C, D)/2
+                map_row = ['I', 'J', 'K', 'L', 'M', 'N']; // (I, J, K, L, M, N)
+                map_col = ['A', 'B', 'C', 'D', 'E', 'F']; // (A, B, C, D, E, F)
 
-                // except_coor = ['A1A1', 'A1A2', 'A1B1', 'A1B2', 'A1C1', 'A1C2', 'A1D1', 'A1D2'
-                // ,'A2A1', 'A2A2', 'A2D1', 'A2D2', 'J1A1', 'J2A1', 'K1A1', 'L1A1', 'L1D2'
-                // ,'L2A1', 'L2A2', 'L2C1', 'L2C2', 'L2D1', 'L2D2'];
+                except_coor = ['IA', 'IB', 'IC', 'ID', 'IE', 'IF',
+                'JA', 'JF', 'KA', 'LA', 'MA', 'MF',
+                'NA', 'NB', 'NC', 'ND', 'NE', 'NF'];
+                MAP_SIZE = RECTANGLE_SIZE*6;
             }
             else{
                 message.reply("undefined map");
@@ -89,13 +88,6 @@ module.exports = class determinePlace extends commando.Command{
         let temp_col = Math.floor(Math.random()*map_col.length);
         let temp_coor = map_row[temp_row] + map_col[temp_col];
         
-        // 생성한 좌표가 except_coor에 해당하는지 검사
-        function isContain(element, ary){
-            for(let x in ary){
-                if(ary[x] == element) return true;
-            }
-            return false;
-        }
 
         let excepted_coor = ''; // 제외된 좌표 저장
         while(isContain(temp_coor, except_coor)){
@@ -124,4 +116,12 @@ module.exports = class determinePlace extends commando.Command{
             });
         });
     }
+}
+
+// 생성한 좌표(element)가 except_coor(ary)에 해당하는지 검사
+function isContain(element, ary){
+    for(let x in ary){
+        if(ary[x] == element) return true;
+    }
+    return false;
 }
--- index.js
+++ index.js
@@ -13,8 +13,8 @@ client.registry
     .registerCommandsIn(__dirname + '/commands');
 
     client.on('ready', () => {
-    console.log('Logged in!');
-});
+        console.log('Logged in! -', getDateTime());
+    });
 
 client.on('message', (message) => {
     if(message.content == 'test'){
@@ -23,3 +23,17 @@ client.on('message', (message) => {
 });
 
 client.login(process.env.KUKBOT_TOKEN); // kuk channel
+
+function getDateTime() {
+
+    let date = new Date();
+
+    let year = date.getFullYear();
+    let month = (date.getMonth() < 11 ? "0" : "") + (date.getMonth()+1);
+    let day  = (date.getDate() < 10 ? "0" : "") + date.getDate();
+    let hour = (date.getHours() < 10 ? "0" : "") + date.getHours();
+    let min  = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+    let sec  = (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
+
+    return year + "/" + month + "/" + day + " - " + hour + ":" + min + ":" + sec;
+}
